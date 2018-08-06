2937da82e4f1e3f3534dab277e07bcfdd97e3591
qubick <qubick.kim@gmail.com>
2018-08-06 16:52:51 -0700

    image library search added

---
+++ event-handler.js
@@ -0,0 +1,115 @@
+$(document).click(function(event) {
+    var text = $(event.target).text();
+    console.log(event.target);
+});
+
+
+$( function() {
+  $( ".draggable" ).draggable({
+    helper: 'clone',
+    cursor: 'move',
+    revert: 'invalid',
+    refrecshPositions: true,
+  });
+
+  $(".droppable").droppable({
+    drop: function(event, ui) {
+      console.log(event)
+    }
+  });
+});
+
+
+
+
+
+function ReturnTypeofGradient(evt){
+  //change gradient type
+  var regtionSelection = parseInt(evt.target.value);
+
+  // var gradientmaterial = generateGradientShaderMaterial(512);
+
+
+  var selectObject= scene.getObjectByName("foot_step_volume");
+  selectObject.material = normalMaterial;
+
+
+  var gradientInput = document.createElement("input");
+
+  gradientInput.type = "range"
+  gradientInput.min = "10";
+  gradientInput.max = "50";
+  gradientInput.value = "25";
+  gradientInput.class = "slider"
+  gradientInput.id = "gradientRange"
+  // gradientInput.onChange = "updateValue(this.value)"
+
+  document.getElementById('sliderlocation').innerHTML = '<br/> Gradient stops: ';
+  document.getElementById('gradientlocation').appendChild(gradientInput);
+
+}
+
+function ReturnReferenceMaterial(evt){
+
+  var shoreAScale = evt.target.value;
+  var input = document.createElement("input");
+
+  input.type = "range"
+  input.min = "10";
+  input.max = "50";
+  input.value = shoreAScale;
+  input.class = "slider"
+  input.id = "softnessRange"
+  input.onChange = "updateValue(this.value)"
+
+  var textInput = document.createElement("text");
+  textInput.value = '<br/> Shore A scale: ' + shoreAScale;
+  textInput.id = "shoreAScaleValue"
+
+  document.getElementById('sliderlocation').appendChild(document.createElement("br"));
+  document.getElementById('sliderlocation').innerHTML = '<br/> Shore A scale: ' + shoreAScale;
+  document.getElementById('sliderlocation').appendChild(input);
+
+}
+
+function ReturnTypeofGradient(evt){
+  //change gradient type
+  var regtionSelection = parseInt(evt.target.value);
+
+  // var gradientmaterial = generateGradientShaderMaterial(512);
+
+
+  var selectObject= scene.getObjectByName("foot_step_volume");
+  selectObject.material = normalMaterial;
+
+
+  var gradientInput = document.createElement("input");
+
+  gradientInput.type = "range"
+  gradientInput.min = "10";
+  gradientInput.max = "50";
+  gradientInput.value = "25";
+  gradientInput.class = "slider"
+  gradientInput.id = "gradientRange"
+  // gradientInput.onChange = "updateValue(this.value)"
+
+  document.getElementById('sliderlocation').innerHTML = '<br/> Gradient stops: ';
+  document.getElementById('gradientlocation').appendChild(gradientInput);
+
+}
+
+
+$( function() {
+  $( ".draggable" ).draggable({
+    helper: 'clone',
+    cursor: 'move',
+    revert: 'invalid',
+    refrecshPositions: true,
+  });
+
+  $(".droppable").droppable({
+    drop: function(event, ui) {
+      console.log(event)
+    }
+  });
+});
---
+++ refer-material.js
@@ -0,0 +1,191 @@
+var reference_material = [
+  {
+    name: "hair-roll",
+    value: 1,
+    softness: 2,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "makeup-sponge",
+    value: 2,
+    softness: 2,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "play-doh",
+    value: 3,
+    softness: 5,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "earplug",
+    value: 4,
+    softness: 7,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "can-cooler",
+    value: 5,
+    softness: 9,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "gummy jelly",
+    value: 6,
+    softness: 10,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "dish-scrubber",
+    value: 7,
+    softness: 11,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "art-eraser",
+    value: 8,
+    softness: 15,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "mouse-pad",
+    value: 9,
+    softness: 16,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "rubber-band",
+    value: 10,
+    softness: 17,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "flipflop-platform",
+    value: 11,
+    softness: 25,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "vinyl-toys",
+    value: 12,
+    softness: 35,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "laptop-protector",
+    value: 13,
+    softness: 30,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "pencil-eraser",
+    value: 14,
+    softness: 40,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "door-seal",
+    value: 15,
+    softness: 55,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "bath-toys",
+    value: 16,
+    softness: 42,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "tooth-paste",
+    value: 17,
+    softness: 44,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "car-tire",
+    value: 18,
+    softness: 60,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "shoe-heels",
+    value: 19,
+    softness: 70,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "bottle-nipple",
+    value: 20,
+    softness: 40,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "smartwatch-band",
+    value: 21,
+    softness: 70,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "chewing-gum",
+    value: 22,
+    softness: "20",
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "leather-belt",
+    value: 23,
+    softness: 80,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "skateboard wheels",
+    value: 24,
+    softness: 78,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "screen-wiper",
+    value: 25,
+    softness: 60,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "shoe-soles",
+    value: 26,
+    softness: 80,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  },
+  {
+    name: "garden-hose",
+    value: 27,
+    softness: 95,
+    selected: false,
+    imageSrc: "assets/imgs/"
+  }
+];
