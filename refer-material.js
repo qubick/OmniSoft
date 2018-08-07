fccc432a671fe238bc23db2a5f10b8b067536ff4
qubick <qubick.kim@gmail.com>
2018-08-06 17:35:51 -0700

    reference material images for selection

--- refer-material.js
+++ refer-material.js
@@ -1,191 +1,218 @@
 var reference_material = [
   {
     name: "hair-roll",
+    text: "hair roll",
     value: 1,
     softness: 2,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/hair-roll.png"
   },
   {
     name: "makeup-sponge",
+    text: "makeup sponge",
     value: 2,
     softness: 2,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/makeup-sponge.png"
   },
   {
     name: "play-doh",
+    text: "play doh",
     value: 3,
     softness: 5,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/play-doh.png"
   },
   {
     name: "earplug",
+    text: "earplug",
     value: 4,
     softness: 7,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/earplug.png"
   },
   {
     name: "can-cooler",
+    text: "can cooler",
     value: 5,
     softness: 9,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/can-cooler.png"
   },
   {
-    name: "gummy jelly",
+    name: "gummy-jelly",
+    text: "gummy jelly",
     value: 6,
     softness: 10,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/gummy-jelly.png"
   },
   {
     name: "dish-scrubber",
+    text: "dish scrubber",
     value: 7,
     softness: 11,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/dish-scrubber.png"
   },
   {
     name: "art-eraser",
+    text: "art eraser",
     value: 8,
     softness: 15,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/art-eraser.jpg"
   },
   {
     name: "mouse-pad",
+    text: "mouse pad",
     value: 9,
     softness: 16,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/mouse-pad.jpg"
   },
   {
     name: "rubber-band",
+    text: "rubber band",
     value: 10,
     softness: 17,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/rubber-band.jpeg"
   },
   {
     name: "flipflop-platform",
+    text: "flip-flop platform",
     value: 11,
     softness: 25,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/flipflop.jpg"
   },
   {
     name: "vinyl-toys",
+    text: "vinyl toys",
     value: 12,
     softness: 35,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/vinyl-toys.jpg"
   },
   {
     name: "laptop-protector",
+    text: "laptop protector insert",
     value: 13,
     softness: 30,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/laptop-protector.jpg"
   },
   {
     name: "pencil-eraser",
+    text: "pencil eraser",
     value: 14,
     softness: 40,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/pencil-eraser.jpg"
   },
   {
     name: "door-seal",
+    text: "door seal",
     value: 15,
     softness: 55,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/door-seal.jpg"
   },
   {
     name: "bath-toys",
+    text: "bath toys",
     value: 16,
     softness: 42,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/bath-toys.jpg"
   },
   {
     name: "tooth-paste",
+    text: "tooth paste tube",
     value: 17,
     softness: 44,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/tooth-paste.jpeg"
   },
   {
     name: "car-tire",
+    text: "car tire",
     value: 18,
     softness: 60,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/car-tire.jpeg"
   },
   {
     name: "shoe-heels",
+    text: "shoe heels",
     value: 19,
     softness: 70,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/shoe-heels.jpg"
   },
   {
     name: "bottle-nipple",
+    text: "bottle-nipple",
     value: 20,
     softness: 40,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/bottle-nipple.jpeg"
   },
   {
     name: "smartwatch-band",
+    text: "smartwatch band",
     value: 21,
     softness: 70,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/smart-watch.jpeg"
   },
   {
     name: "chewing-gum",
+    text: "chewing gum",
     value: 22,
     softness: "20",
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/chewing-gum.jpeg"
   },
   {
     name: "leather-belt",
+    text: "leather belt",
     value: 23,
     softness: 80,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/leather-belt.jpeg"
   },
   {
-    name: "skateboard wheels",
+    name: "skateboard-wheels",
+    text: "skateboard wheels",
     value: 24,
     softness: 78,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/skateboard.jpg"
   },
   {
     name: "screen-wiper",
+    text: "screen wiper blades",
     value: 25,
     softness: 60,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/screen-wiper.png"
   },
   {
     name: "shoe-soles",
+    text: "shoe soles",
     value: 26,
     softness: 80,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/shoe-sole.png"
   },
   {
     name: "garden-hose",
+    text: "garden hose",
     value: 27,
     softness: 95,
     selected: false,
-    imageSrc: "assets/imgs/"
+    imageSrc: "assets/imgs/garden-hose.jpg"
   }
 ];
