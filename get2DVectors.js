475ada74732c6242774bfe98348cb6f3fa25dd05
qubick <qubick.kim@gmail.com>
2018-08-17 11:24:55 -0700

    2D vector vertices from 3D object by cutting plane in xy (Issue #23)

---
+++ get2DVectors.js
@@ -0,0 +1,85 @@
+//create a plane to intersect the object
+// var plane = new THREE.PlaneGeometry();
+// var mathPlane = new THREE.Plane();
+
+var pointsOfIntersection = new THREE.Geometry();
+var pointOfIntersection = new THREE.Vector3();
+var pointMaterial = new THREE.PointsMaterial({
+  size:.5,
+  color: "yellow"
+});
+
+var a = new THREE.Vector3()
+  ,b = new THREE.Vector3()
+  ,c = new THREE.Vector3();
+
+var planePointA = new THREE.Vector3()
+    ,planePointB = new THREE.Vector3()
+    ,planePointC = new THREE.Vector3();
+
+var lineAB, lineBC, lineCA;
+
+// var objGeom = new THREE.DodecahedronGeometry(10, 0);
+// var obj = new THREE.Mesh(objGeom, new THREE.MeshBasicMaterial({
+//   color: "green",
+//   wireframe: true
+// }));
+// // obj.rotation.z = Math.PI / 10;
+// // obj.position.set(0, 3.14, 0);
+// scene.add(obj);
+
+function cutInPlaneToGet2DVectors(){
+  console.log("in get2DVectors.js: ", target3DObject);
+
+  var planeGeom = new THREE.PlaneGeometry(30, 30);
+  var mathPlane = new THREE.Plane();
+
+  planeGeom.rotateX(-Math.PI / 2);
+
+  var plane = new THREE.Mesh(planeGeom, new THREE.MeshBasicMaterial({
+    color: "lightgray",
+    transparent: true,
+    opacity: 0.75,
+    side: THREE.DoubleSide
+  }));
+
+  plane.position.y = -3.14;
+  // plane.rotation.x = Math.PI / 5;
+  scene.add(plane);
+
+  //create plane
+  plane.localToWorld(planePointA.copy(plane.geometry.vertices[plane.geometry.faces[0].a]));
+  plane.localToWorld(planePointB.copy(plane.geometry.vertices[plane.geometry.faces[0].b]));
+  plane.localToWorld(planePointC.copy(plane.geometry.vertices[plane.geometry.faces[0].c]));
+  mathPlane.setFromCoplanarPoints(planePointA, planePointB, planePointC);
+
+  sphereRegion.geometry.faces.forEach( (face) => {
+    sphereRegion.localToWorld(a.copy(sphereRegion.geometry.vertices[face.a]));
+    sphereRegion.localToWorld(b.copy(sphereRegion.geometry.vertices[face.b]));
+    sphereRegion.localToWorld(c.copy(sphereRegion.geometry.vertices[face.c]));
+
+    lineAB = new THREE.Line3(a, b);
+    lineBC = new THREE.Line3(b, c);
+    lineCA = new THREE.Line3(c, a);
+
+    setPointOfIntersection(lineAB, mathPlane);
+    setPointOfIntersection(lineBC, mathPlane);
+    setPointOfIntersection(lineCA, mathPlane);
+  });
+
+  var points = new THREE.Points(pointsOfIntersection, pointMaterial);
+  scene.add(points);
+}
+
+function setPointOfIntersection(line, plane){
+
+  pointOfIntersection = plane.intersectLine(line);
+  if(pointOfIntersection){
+    pointsOfIntersection.vertices.push(pointOfIntersection.clone())
+  }
+}
+
+function drawIntersectionPoints(){
+
+
+}
